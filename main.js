d93ad8470b7b3f7a186076c9deacbcddb6d98f7a
Pankaj gupta <pankajgup82@gmail.com>
2026-06-03 18:17:19 +0530

    ch

--- main.js
+++ main.js
@@ -66,14 +66,13 @@ ffmpeg("./assets/background.mp4")
       text: fact,
       fontsize: 40,
       fontcolor: "white",
-      // Linux/Ubuntu par default font path taaki fail na ho
-      fontfile: "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 
+      fontfile: "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf",
       x: "(w-text_w)/2",
       y: "(h-text_h)/2"
     }
   }
 ])
-  .output("./output/video.mp4")
+.output("./output/video.mp4")
   .on("end", () => {
 
     console.log("✅ Video Generated");
